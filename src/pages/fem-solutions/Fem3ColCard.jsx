eb4250a8158add74f76bc46da53b1f0873b97981
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2023-01-03 23:58:26 +0800

    fix UI of fem challenge 06 - 3 column card

--- src/pages/fem-solutions/Fem3ColCard.jsx
+++ src/pages/fem-solutions/Fem3ColCard.jsx
@@ -9,52 +9,50 @@ import {
 
 //-------------------------------------------------------------------
 
+const ColumnCard = ({ columnTitle, description, icon, bgColor, borderRd }) => (
+  <div
+    className={`w-11/12 xs:w-[300px]  text-fem-06-primary-color p-11 ${borderRd} ${bgColor}`}
+  >
+    <img src={icon} alt="icon-sedan" />
+    <h1 className="font-bigshoulder font-extrabold text-4xl uppercase py-8">
+      {columnTitle}
+    </h1>
+    <p className="text-fem-06-secondary-color pb-14">{description}</p>
+    <button className="bg-fem-06-primary-color text-fem-06-orange font-bold px-8 py-3 rounded-full hover:bg-transparent hover:border-2 hover:border-fem-06-primary-color hover:text-fem-06-primary-color my-3">
+      Learn More
+    </button>
+  </div>
+);
+
 const Fem3ColCard = () => {
   return (
     <div className="flex relative bg-fem-06-primary-color h-full py-20 lg:py-0 lg:h-screen w-screen justify-center items-center text-gray-900 overflow-hidden">
-      <div className="flex flex-col lg:flex-row items-center">
-        <div className="w-10/12 md:w-5/12 lg:w-72 bg-fem-06-orange text-fem-06-primary-color p-11 rounded-t-xl lg:rounded-l-xl lg:rounded-r-none">
-          <img src={fem3colIconSedan} alt="icon-sedan" />
-          <h1 className="font-bigshoulder font-extrabold text-4xl uppercase py-8">
-            Sedans
-          </h1>
-          <p className="text-fem-06-secondary-color pb-14">
-            Choose a sedan for its affordability and excellent fuel economy.
-            Ideal for cruising in the city or on your next road trip.
-          </p>
-          <button className="bg-fem-06-primary-color text-fem-06-orange font-bold px-8 py-3 rounded-full hover:bg-transparent hover:border-2 hover:border-fem-06-primary-color hover:text-fem-06-primary-color my-3">
-            Learn More
-          </button>
-        </div>
-
-        <div className="w-10/12 md:w-5/12 lg:w-72 bg-fem-06-dark-cyan1 text-fem-06-primary-color p-11">
-          <img src={fem3colIconSuvs} alt="icon-suvs" />
-          <h1 className="font-bigshoulder font-extrabold text-4xl uppercase py-8">
-            Suvs
-          </h1>
-          <p className="text-fem-06-secondary-color pb-14">
-            Take an SUV for its spacious interior, power, and versatility.
-            Perfect for your next family vacations and off-road adventures.
-          </p>
-          <button className="bg-fem-06-primary-color text-fem-06-dark-cyan1 font-bold px-8 py-3 rounded-full hover:bg-transparent hover:border-2 hover:border-fem-06-primary-color hover:text-fem-06-primary-color hover:translate-0 my-3">
-            Learn More
-          </button>
-        </div>
-
-        <div className="w-10/12 md:w-5/12 lg:w-72 bg-fem-06-dark-cyan2 text-fem-06-primary-color p-11 rounded-b-xl lg:rounded-r-xl lg:rounded-l-none">
-          <img src={fem3colIconLuxury} alt="icon-suvs" />
-          <h1 className="font-bigshoulder font-extrabold text-4xl uppercase py-8">
-            Luxury
-          </h1>
-          <p className="text-fem-06-secondary-color pb-14">
-            Cruise in the best car brands without the bloated prices. Enjoy the
-            enhanced comfort of a luxury rental and arrive in style.
-          </p>
-          <button className="bg-fem-06-primary-color text-fem-06-dark-cyan2 font-bold px-8 py-3 rounded-full hover:bg-transparent hover:border-2 hover:border-fem-06-primary-color hover:text-fem-06-primary-color my-3">
-            Learn More
-          </button>
-        </div>
-      </div>
+      {/* == challenge solution == */}
+      <main className="flex flex-col lg:flex-row items-center">
+        <ColumnCard
+          columnTitle="Sedans"
+          description="Choose a sedan for its affordability and excellent fuel economy.
+            Ideal for cruising in the city or on your next road trip."
+          icon={fem3colIconSedan}
+          bgColor="bg-fem-06-orange"
+          borderRd="rounded-t-xl lg:rounded-l-xl lg:rounded-r-none"
+        />
+        <ColumnCard
+          columnTitle="Suvs"
+          description="Take an SUV for its spacious interior, power, and versatility.
+            Perfect for your next family vacations and off-road adventures."
+          icon={fem3colIconSuvs}
+          bgColor="bg-fem-06-dark-cyan1"
+        />
+        <ColumnCard
+          columnTitle="Luxury"
+          description="Cruise in the best car brands without the bloated prices. Enjoy the
+            enhanced comfort of a luxury rental and arrive in style."
+          icon={fem3colIconLuxury}
+          bgColor="bg-fem-06-dark-cyan2"
+          borderRd="rounded-b-xl lg:rounded-r-xl lg:rounded-l-none"
+        />
+      </main>
 
       {/* == back to homepage button == */}
       <Link
